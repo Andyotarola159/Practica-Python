5b2c29c6e2ff17f6c3acc6fba460a31b4b879450
Andyotarola <andyotarola8@gmail.com>
2017-03-24 19:55:30 -0500

    archivo python

---
+++ hola.py
@@ -0,0 +1,99 @@
+milista=["a","b","c","d","e","a"]
+mitubla=tuple(milista)
+
+la, lb, lc, ld, le, lf=mitubla
+ 
+print(ld,lb,le)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#mitupla=tuple(milista)  #convertir una lista en una tupla
+#print(mitupla.count("a")) #sirve para saber cuentas veces se repite un elemento
+#print(len(mitupla)) #sirve para saber cuantos elementos tiene una lista o una tupla
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#milista=list(mitupla) #sirve para convertir una tupla en una lista
+
+#print("e" in mitupla)  #sirve para saber si un elemento se encuentra dentro de una tubla u lista
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#lista=["a","b","c","d"]
+#lista2=["e","f","g","h"]
+#lista3=lista+lista2
+#print(lista3.index("h"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#lista.extend(["e","f","g"])  #sirve para agregar los elementos que quieras a una lista empezando desde el ultimo elemento
+#lista.remove("b")
+#lista.insert(2,"k")    #sirve para insetar un elementp donde quieras
+#lista.pop()   #sirve para eliminar el ultimo de una lista
+
+
+#lista.append("h")       #sirve para poner un elemento al ultimo de lista
+
+
+#print(lista[:])
+#print(lista.index("g"))  #sirve para saber en que lugar de lista se encuentra el elemento 
+#print(lista[:])   #te muestra todo los elementos de la lista
+#print("l" in lista) #sirve para sabe si un elemento se encuentra en la lista
